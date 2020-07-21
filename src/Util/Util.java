e3e341ca5228b1ecb08897b285a2e5441a9c0f15
shindongsub <ehdtjq1982@gmail.com>
2020-07-22 03:02:21 +0900

    1 commit

---
+++ src/Util/Util.java
@@ -0,0 +1,16 @@
+package Util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	public static String getCurrentDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yy.MM.dd HH:mm:ss");
+		Date cal = new Date();
+		String cal1 = sdf.format(cal);
+		System.out.println(cal1);
+		return cal1;
+
+	}
+
+}
